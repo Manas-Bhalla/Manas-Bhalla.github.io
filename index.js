d898d1b69be76671cee6a4337961d7b79e1ef80c
Manas-Bhalla <manasbhalla1983@gmail.com>
2025-05-09 17:35:33 -0700

    reorganized files into folders, set up project 3 folder, fixed loadHeader to remove repetition in files

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ function myFunction() {
     
 }
 
+function loadHeader() { // loading header, css, and more
+    //loading the header
+    fetch("/header.html")
+        .then(response => response.text())
+        .then(data => {
+            const headerElement = document.getElementById("header");
+            headerElement && (headerElement.innerHTML = data);
+        });
+        
+    //loading the CSS
+    const cssLink = document.createElement("link");
+    cssLink.rel = "stylesheet";
+    cssLink.href = "/styles.css"; // Path to your CSS file
+    cssLink.type = "text/css";
+
+    // checking if CSS is loaded already
+    if (!document.querySelector(`link[href="/styles.css"]`)) {
+        document.head.appendChild(cssLink);
+    }
+
+    //icon
+    const faviconLink = document.createElement("link");
+    faviconLink.rel = "icon";
+    faviconLink.type = "image/x-icon";
+    faviconLink.href = "/peon.png"; // Path to your favicon
+
+    //check if already loaded
+    if (!document.querySelector(`link[href="/peon.png"]`)) {
+        document.head.appendChild(faviconLink);
+    }
+}
